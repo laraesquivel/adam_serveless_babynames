852e84a71b089e21385c8493b3cea62a14378f85
laraesquivel <laraesquivelsantos@gmail.com>
2024-02-12 15:42:28 -0300

    get names

--- src/index.py
+++ src/index.py
@@ -9,12 +9,8 @@ app = FastAPI()
 URI = "mongodb+srv://laraesquivel:babynames@babys.iuiuuvp.mongodb.net/"
 
 
-#handler = Mangum(app)  # handler for deploy FastAPI to lambdas
-
-@app.on_event("startup")
-def startup_db_client():
-    app.mongodb_client = MongoClient(URI)
-    app.database = app.mongodb_client['babynames']
+app.mongodb_client = MongoClient(URI)
+app.database = app.mongodb_client['babynames']  
 
 app.include_router(gets_router)
 
