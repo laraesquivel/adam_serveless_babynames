ae34c01c7eb8c45a032e3792c5957d398591e432
laraesquivel <laraesquivelsantos@gmail.com>
2024-02-27 17:44:34 -0300

    newfunction

--- src/gets.py
+++ src/gets.py
@@ -26,19 +26,15 @@ def get_names(request: Request, name: str = None):
     # Send a ping to confirm a successful connection
     if not name:
         raise HTTPException(status_code=400, detail="Por favor, forneça um nome para pesquisar na lista de nomes.")
-    if name:
-        babynames = request.app.database["names"]
-        print(babynames)
-        result = json_util.dumps(babynames.find_one({'name': name}), default = str)
-        if result:
-            print(result)
-            return JSONResponse(content=result, status_code=200, media_type="application/json")
-        result = json_util.dumps(babynames.find_one({'closers' : name}))
-        if result:
-            return JSONResponse(content=result,status_code=200, media_type="application/json")
-        
+    
+    babynames = request.app.database["names"]
+    result = babynames.find_one({'name': name})
+    
+    if result:
+        result_json = json_util.dumps({'found': True, 'data': result}, default=str)
+        return JSONResponse(content=result_json, status_code=200, media_type="application/json")
 
-    return JSONResponse(content=json_util.dumps({'message' : "We dont find it!"}), status_code=404, media_type="application/json")
+    return JSONResponse(content=json_util.dumps({'found': False, 'message': "Name not found!"}), status_code=404, media_type="application/json")
 
 @router.get("/getRecPhrase/{user_id}")
 def get_rec_phrase(request : Request, user_id : str = None):
