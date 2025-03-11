313a20b018768931f77ce09f14d5a963e540e8f9
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2025-03-11 09:39:09 -0300

    Update gets.py

--- src/gets.py
+++ src/gets.py
@@ -126,7 +126,8 @@ async def generate_recommendations(request : Request):
                     names[nr_action] +=peso
 
         names_to_update[name] = names
-    return {"names_to_update": names_to_update}
+        print(names_to_update)
+    return {"message" : "Recomendações geradas com sucesso"}
     # """
     # Gera recomendações temporárias para um usuário com base na interação de outros usuários.
     # """
